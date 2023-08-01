05c88fe8fb740dd8d51a40c8714e80935d0325b6
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2023-08-01 21:39:32 +0200

    remove unused code from preload

--- kernel/preload.js
+++ kernel/preload.js
@@ -1,10 +1,1 @@
 require("./rpPreload.js");
-// const fs = require("fs");
-// async function sleep(ms) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
-// sleep(3000).then(() => {
-//   let script = document.createElement("script");
-//   script.textContent = fs.readFileSync(__dirname + "/" + "rpRenderer.js", "utf8");
-//   document.body.append(script);
-// });
